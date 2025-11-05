e8305dd2f7d31ebaccb2be6693d4f0d03c9cb100
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-11-05 09:35:03 +0530

    **&^^

--- newdb/models.py
+++ newdb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Newdb(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=3)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
