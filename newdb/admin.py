bf04c302aac109228e5853a99819f72b7b3abd4c
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-11-05 09:36:20 +0530

    **&&&&&&&^^^^

--- newdb/admin.py
+++ newdb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Newdb
 
-# Register your models here.
+@admin.register(Newdb)
+class NewdbAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'email')   # columns shown in admin list view
+    search_fields = ('name', 'email')         # adds a search box
+
