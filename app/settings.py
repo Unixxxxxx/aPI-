079cec5dc547f6aadfea87d9e87ee2c36d2707e4
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-11-10 05:19:18 +0530

    *

--- app/settings.py
+++ app/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
