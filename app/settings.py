02135e48a3aeba62ff8a028d5b427bc7888b565f
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-11-17 22:00:34 +0530

    *

--- app/settings.py
+++ app/settings.py
@@ -86,6 +86,25 @@ WSGI_APPLICATION = 'app.wsgi.application'
     #)
 #}
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'your_db_name',
+    #    'USER': 'your_username',
+        #'PASSWORD': 'your_password',
+        #'HOST': 'localhost',     # or server IP
+        #'PORT': '3306',
+    #}
+#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
