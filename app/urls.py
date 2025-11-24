914277d0a70f084e568360a2e75c42d8eeaff09d
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-11-24 16:20:44 +0530

    && add 404__^

--- app/urls.py
+++ app/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('',include('itinerary.urls')),
 ] 
 
+
+handler404 = 'app.views.custom_404'
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
