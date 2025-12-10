dcb992c01d1424f12047d94718dd70d7953a7de4
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-12-10 16:38:12 +0530

    End

--- itinerary/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('kedarnath/', views.kedarnath, name='kedarnath'),
-]
-
--- itinerary/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def kedarnath(request):
-    return render(request, 'itinerary/index.html', {})
-
